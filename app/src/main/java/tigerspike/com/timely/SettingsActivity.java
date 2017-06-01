722768383040c329195fc20bf5b7db9ab816a668
anasabu <anas.abubacker@tigerspike.com>
2017-06-01 11:30:16 +0400

    Choosing LM

--- app/src/main/java/tigerspike/com/timely/SettingsActivity.java
+++ app/src/main/java/tigerspike/com/timely/SettingsActivity.java
@@ -1,14 +1,21 @@
 package tigerspike.com.timely;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
+
+import com.afollestad.materialdialogs.MaterialDialog;
+import com.afollestad.materialdialogs.simplelist.MaterialSimpleListAdapter;
+import com.afollestad.materialdialogs.simplelist.MaterialSimpleListItem;
 
 public class SettingsActivity extends AppCompatActivity {
 
+    private TextView mChooseLineManager;
     private TextView mTextMessage;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
@@ -37,9 +44,52 @@ public class SettingsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
-//        mTextMessage = (TextView) findViewById(R.id.message);
+        mChooseLineManager = (TextView) findViewById(R.id.choose_your_lm);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        mChooseLineManager.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                prepareAndShowLineManagers();
+            }
+        });
+    }
+
+
+    private void prepareAndShowLineManagers() {
+        final MaterialSimpleListAdapter adapter = new MaterialSimpleListAdapter(new MaterialSimpleListAdapter.Callback() {
+            @Override
+            public void onMaterialListItemSelected(MaterialDialog dialog, int index, MaterialSimpleListItem item) {
+                // TODO
+                mChooseLineManager.setText(item.getContent());
+            }
+        });
+
+        adapter.add(new MaterialSimpleListItem.Builder(this)
+                .content("Adrian.roney@tigerspike.com")
+//                .icon(R.drawable.ic_account_circle)
+                .backgroundColor(Color.WHITE)
+                .build());
+        adapter.add(new MaterialSimpleListItem.Builder(this)
+                .content("steve.burrows@tigerspike.com")
+//                .icon(R.drawable.ic_account_circle)
+                .backgroundColor(Color.WHITE)
+                .build());
+        adapter.add(new MaterialSimpleListItem.Builder(this)
+                .content("ronan.donohoe@tigerspike.com")
+//                .icon(R.drawable.ic_content_add)
+                .iconPaddingDp(8)
+                .build());
+        adapter.add(new MaterialSimpleListItem.Builder(this)
+                .content("andy.boyle@tigerspike.com")
+//                .icon(R.drawable.ic_content_add)
+                .iconPaddingDp(8)
+                .build());
+
+        new MaterialDialog.Builder(this)
+                .title("Choose Your Line Manager")
+                .adapter(adapter, null)
+                .show();
     }
 
 }
